5b2186bb6e1b0ecf63cb42452b13dfb23a8ff4e7
Isonade2 <c1004sos@naver.com>
2024-01-23 01:08:54 +0900

    4. 스프링 빈과 의존관계 5. MVC개발 6. DB 접근 기술 7. AOP

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,10 +3,16 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -15,10 +21,18 @@ public class MemberService {
     }
 
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            //같은 이름이 있는 중복 회원X
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs);
+        }
     }
 
     private void validateDuplicateMember(Member member) {
