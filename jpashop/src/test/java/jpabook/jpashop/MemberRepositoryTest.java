2a392c9060f49f0c3ab7f2fde701210744cf7dbf
Isonade2 <c1004sos@naver.com>
2024-03-19 23:04:47 +0900

    스프링 부트와 JPA 활용 1 복습 2. 도메인 분석 설계 3. 애플리케이션 구현 준비

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
@@ -15,12 +18,20 @@ class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @PersistenceContext
+    EntityManager em;
     @Test
     @Rollback(false)
     public void test(){
         Member member = new Member();
-        member.setUsername("memberB");
+        member.setUsername("memberA");
         Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
 
     }
 
